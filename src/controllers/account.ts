c459e798c40c24cd64d2c86f72eb2fe33ec1818c
DakshDev <dakshkishore99@gmail.com>
2025-10-22 06:34:15 +0500

    cookie secure true

--- src/controllers/account.ts
+++ src/controllers/account.ts
@@ -73,7 +73,7 @@ async function createAccount(req: Request, res: Response) {
     return res
       .cookie("token", token, {
         httpOnly: true,
-        secure: _env.NODE_ENV === "production",
+        secure: true,
         sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
@@ -114,7 +114,7 @@ async function loginAccount(req: Request, res: Response) {
     return res
       .cookie("token", token, {
         httpOnly: true,
-        secure: _env.NODE_ENV === "production",
+        secure: true,
         sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
